0044c775dac555bcd4a74d35efc2e16f35199690
Belle Hannah Sebial <114854900+bellesebial@users.noreply.github.com>
2022-11-17 13:42:31 +0800

    sdsd

---
+++ tekarch/src/main/java/com/ppg/tekarch/TekarchApplication.java
@@ -0,0 +1,13 @@
+package com.ppg.tekarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekarchApplication.class, args);
+	}
+
+}
