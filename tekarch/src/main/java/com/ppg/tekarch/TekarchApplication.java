db1ce24522df654a826e92a12cfadad3ab3394fb
Belle Hannah Sebial <114854900+bellesebial@users.noreply.github.com>
2022-11-17 14:58:52 +0800

    gegeg

--- tekarch/src/main/java/com/ppg/tekarch/TekarchApplication.java
+++ tekarch/src/main/java/com/ppg/tekarch/TekarchApplication.java
@@ -3,7 +3,7 @@ package com.ppg.tekarch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.ppg.tekarch.Controller", "com.ppg.tekarch.Service", "com.ppg.tekarch.TekarchApplication"})
 public class TekarchApplication {
 
 	public static void main(String[] args) {
